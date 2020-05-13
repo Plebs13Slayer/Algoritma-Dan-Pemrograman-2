cb8daab495f9a6a7746fc52a719a5882d0d9c6e5
Akaneee <fahrizal.dina@gmail.com>
2020-05-13 11:28:06 +0800

    Update InsertionSort.cpp

--- Tugas Sorting/InsertionSort.cpp
+++ Tugas Sorting/InsertionSort.cpp
@@ -1,15 +1,14 @@
-#include <bits/stdc++.h> 
-using namespace std; 
+#include <math.h> 
+#include <stdio.h> 
 
 void insertionSort(int arr[], int n) 
 { 
 	int i, key, j; 
-	for (i = 1; i < n; i++) 
-	{ 
+	for (i = 1; i < n; i++) { 
 		key = arr[i]; 
 		j = i - 1; 
 
-		{ 
+		while (j >= 0 && arr[j] > key) { 
 			arr[j + 1] = arr[j]; 
 			j = j - 1; 
 		} 
@@ -21,18 +20,19 @@ void printArray(int arr[], int n)
 { 
 	int i; 
 	for (i = 0; i < n; i++) 
-		cout << arr[i] << " "; 
-	cout << endl; 
+		printf("%d ", arr[i]); 
+	printf("\n"); 
 } 
 
 int main() 
 { 
 	int arr[] = { 12, 11, 13, 5, 6 }; 
-	int n = sizeof(arr) / sizeof(arr[0]); 
-
-	insertionSort(arr, n); 
-	printArray(arr, n); 
+	int n = sizeof(arr) / sizeof(arr[0]);
+	printf("Array sebelum sorting :\n");
+	printArray(arr,n);
+	insertionSort(arr, n);
+	printf("Array setelah sorting :\n");
+	printArray(arr, n);
 
 	return 0; 
 }
-
