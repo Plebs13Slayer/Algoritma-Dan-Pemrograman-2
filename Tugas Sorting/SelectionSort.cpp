66050ef6e437d156ecc8e2a1c4a9039eb523009d
Akaneee <fahrizal.dina@gmail.com>
2020-05-13 11:50:23 +0800

    Update SelectionSort.cpp

--- Tugas Sorting/SelectionSort.cpp
+++ Tugas Sorting/SelectionSort.cpp
@@ -16,7 +16,7 @@ void selectionSort(int arr[], int n)
 	{ 
 		min_idx = i; 
 		for (j = i+1; j < n; j++) 
-		if (arr[j] < arr[min_idx]) 
+		if (arr[j] < arr[min_idx]) //(<)Ascending (>)Descending
 			min_idx = j; 
 
 		swap(&arr[min_idx], &arr[i]); 
