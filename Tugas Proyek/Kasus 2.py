307c619d8b1bdcf4b1491a1b8590bff0eb320e7f
Akaneee <fahrizal.dina@gmail.com>
2020-07-08 23:22:15 +0800

    Update Kasus 2.py
    Bug fixes and more pythonic way of writing

--- Tugas Proyek/Kasus 2.py
+++ Tugas Proyek/Kasus 2.py
@@ -4,7 +4,7 @@
 limit = int(input('Input Limit : ')) # Batas Data
 karyawan = {} # Deklarasi Dictionary
 
-def Line():
+def line():
     print('========================================')
 
 # Function Kalkulasi Gaji
@@ -29,45 +29,47 @@ def Salary(par1,par2):
             return int(par2 * 5000 * 1.5)
         else:
             return par2 * 5000
+        
+# Output Data Terpilih
+def found():
+    gaji = Salary(int(karyawan[item][1]), int(karyawan[item][2]))
+    # Output Data Terpilih
+    line()
+    print('Nama : {}'.format(karyawan[item][0]))
+    print('Golongan : {}'.format(karyawan[item][1]))
+    print('Jam kerja : {}'.format(karyawan[item][2]))
+    print('Gaji : Rp.{}'.format(gaji))
+    line()
 
 # Loop Input
 for x in range(limit):
-    Line()
-    print('data ke-'+ str(x+1))
+    line()
+    print('data ke-{0}'.format(x+1))
     NIK = input('Masukkan NIK : ')
     Name = input('Masukkan Nama : ')
     Gol = input('Masukkan Golongan : ')
     JamKerja = input('Masukkan Jam Kerja : ')
     karyawan[NIK] = [Name,Gol,JamKerja]
-    Line()
 
 # Output Seluruh Data Yang Telah Dimasukkan
-Line()
-print('jumlah data yang tersimpan : ' + str(len(karyawan)))
+line()
+print('jumlah data yang tersimpan : {}'.format(len(karyawan)))
 print('Data yang tersimpan : ')
 for x in karyawan:
-    print('NIK : ' + x + ' Nama : ' + karyawan[x][0] + ' Golongan : ' + karyawan[x][1] + ' Jam Kerja : ' + karyawan[x][2])
-Line()
+    print('NIK : {0}  Nama : {1}  Golongan : {2}  Jam Kerja : {3}'.format(x, karyawan[x][0], karyawan[x][1], karyawan[x][2]))
+line()
 
 flag = 1 # Deklarasi Check
-
 while flag == 1:
     item = input('Masukkan NIK Yang Akan Ditampilkan : ')
-    # Output Data Terpilih
-    for key in karyawan.keys() :
-        if key == item :
-            gaji = Salary(int(karyawan[key][1]), int(karyawan[key][2]))
-            Line()
-            print('Nama : ' + karyawan[key][0])
-            print('Golongan : ' + karyawan[key][1])
-            print('Jam kerja : ' + karyawan[key][2])
-            print('Gaji : Rp.' + str(gaji))
-            Line()
-        else:
-            print('Data tidak ditemukan')
-            Line()
+    # Mencari Key Dalam Dictionary
+    if item in karyawan:
+        found()
+    else:
+        print('Data Tidak Ditemukan')
+        line()
     # Check Option
     flag = int(input('apakah anda ingin menampilkan data lain ? \n1.Iya\n2.Tidak\n>>'))
-    Line()
+    line()
     if flag != 1:
         break
